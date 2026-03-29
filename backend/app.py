f0fc248cc7cf1948abf7280c84f759f3c87980a3
tinkpad <jurinho@hotmail.de>
2026-03-29 15:23:11 +0200

    Now using fancy Models instead of dict/list + geo data :'D

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import random
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-from bahnhofs import BHF_ID_MAP, BHF_LIST, ID_BHF_MAP, ID_LIST
+from bahnhofs import BHF_LIST, NAME_MAP, BahnhofView
 
 app = FastAPI()
 
@@ -12,27 +12,6 @@ class TrainView(BaseModel):
     train_name: str
     train_id: str
     delay: int
-
-
-class BahnhofView(BaseModel):
-    bhf_name: str
-    bhf_id: str
-    geo_lat: float = 0.0
-    geo_lon: float = 0.0
-
-    @classmethod
-    def from_id(cls, bahnhof_id: str) -> BahnhofView:
-        bahnhof_name = ID_BHF_MAP.get(bahnhof_id)
-        if bahnhof_name is None:
-            raise ValueError(f"ID {bahnhof_id} not found!")
-        return BahnhofView(bhf_name=bahnhof_name, bhf_id=bahnhof_id)
-
-    @classmethod
-    def from_name(cls, bahnhof_name: str) -> BahnhofView:
-        bahnhof_id = BHF_ID_MAP.get(bahnhof_name)
-        if bahnhof_id is None:
-            raise ValueError(f"ID {bahnhof_name} not found!")
-        return BahnhofView(bhf_name=bahnhof_name, bhf_id=bahnhof_id)
 
 
 class FlipView(BaseModel):
@@ -44,30 +23,21 @@ class FlipView(BaseModel):
 def perform_flip(bahnhof: str | None = None) -> FlipView:
 
     if bahnhof is None:
-        bahnhof_id = random.choice(ID_LIST)  # noqa: S311
+        bahnhof_view = random.choice(BHF_LIST)  # noqa: S311
     else:
-        bahnhof_id = BHF_ID_MAP.get(bahnhof)
-        if bahnhof_id is None:
+        bahnhof_view = NAME_MAP.get(bahnhof)
+        if bahnhof_view is None:
             raise HTTPException(status_code=404, detail=f"Bahnhof '{bahnhof}' not found!")
-    bahnhof_name = ID_BHF_MAP[bahnhof_id]
 
     # TODO scrape delays for given bahnhof
-
-    bahnhof_view = BahnhofView(bhf_id=bahnhof_id, bhf_name=bahnhof_name)
     train_view = TrainView(train_name="Sonnenallee Express", train_id="ICE-123", delay=1)
     return FlipView(bahnhof=bahnhof_view, next_train=train_view)
 
 
 @app.get("/list")
-def get_list() -> list[str]:
+def get_list() -> list[BahnhofView]:
 
     return BHF_LIST
-
-
-@app.get("/map")
-def get_map() -> dict[str, str]:
-
-    return ID_BHF_MAP
 
 
 @app.get("/bhf")
--- backend/src/bahnhofs.py
+++ backend/src/bahnhofs.py
@@ -1,50 +1,67 @@
 # https://www.dbinfrago.com/resource/blob/13131484/1ce28c8767d41904e597e6f99e0bba39/INB-2026-Anlage-5-1b-data.pdf
+# https://stellwerke.info/stw/
 
-ID_BHF_MAP = {
-    "AA": "Hamburg-Altona",
-    "AH": "Hamburg Hbf",
-    "AHAR": "Hamburg-Harburg",
-    "BFBI": "Flughafen BER",
-    "BFRI": "Berlin Friedrichstraße",
-    "BGS": "Berlin-Gesundbrunnen",
-    "BHF": "Berlin Ostbahnhof",
-    "BL": "Berlin Hauptbahnhof",
-    "BLO": "Berlin-Lichtenberg",
-    "BOKN": "Berlin Ostkreuz (Ringbahn-F)",
-    "BOKR": "Berlin Ostkreuz (Stadtbahn-F)",
-    "BPAF": "Berlin Südkreuz",
-    "BSPD": "Berlin-Spandau",
-    "BPD": "Potsdam Hbf",
-    "BZOO": "Berlin Zoologischer Garten",
-    "EDG": "Duisburog Hbf",
-    "EDO": "Dortmund Hbf",
-    "EE": "Essen Hbf",
-    "EMSTP": "Münster (Westf) Pbf",
-    "FF": "Frankfurt (Main) Hbf",
-    "FFLF": "Frankfurt am Main Flughafen Fernbahnhof",
-    "FMZ": "Mainz Hbf",
-    "HB": "Bremen Hbf",
-    "HH": "Hannover Hbf",
-    "KB": "Bonn Hbf",
-    "KD": "Düsseldorf Hbf",
-    "KK": "Köln Hbf",
-    "KKDZ": "Köln Messe/Deutz",
-    "LL": "Leipzig Hbf",
-    "MH": "München Hbf",
-    "MKA": "Mü Karlsplatz",
-    "MMP": "München Marienplat",
-    "MOP": "München Ost Pbf",
-    "MP": "München-Pasing",
-    "NN": "Nürnberg Hbf",
-    "RF": "Freiburg (Breisgau) Hbf",
-    "RK": "Karlsruhe Hbf",
-    "RKAB": "Karlsruhe Albtalbahnhof",
-    "RM": "Mannheim Hbf",
-    "TS": "Stuttgart Hbf",
-}
+from pydantic import BaseModel
 
 
-BHF_ID_MAP = {v: k for k, v in ID_BHF_MAP.items()}
+class BahnhofView(BaseModel):
+    bhf_name: str
+    bhf_id: str
+    geo_lat: float = 0.0
+    geo_lon: float = 0.0
 
-BHF_LIST = list(BHF_ID_MAP.keys())
-ID_LIST = list(ID_BHF_MAP.keys())
+    @classmethod
+    def from_id(cls, bahnhof_id: str) -> BahnhofView:
+        if bahnhof_id not in ID_MAP:
+            raise ValueError(f"ID {bahnhof_id} not found!")
+        return ID_MAP[bahnhof_id]
+
+    @classmethod
+    def from_name(cls, bahnhof_name: str) -> BahnhofView:
+        if bahnhof_name not in ID_MAP:
+            raise ValueError(f"Name {bahnhof_name} not found!")
+        return ID_MAP[bahnhof_name]
+
+BHF_LIST =[
+    BahnhofView(bhf_id="AA", bhf_name="Hamburg-Altona", geo_lat=53.55657, geo_lon=9.93598),
+    BahnhofView(bhf_id="AH", bhf_name="Hamburg Hbf", geo_lat=53.54739, geo_lon=10.00916),
+    BahnhofView(bhf_id="AHAR", bhf_name="Hamburg-Harburg", geo_lat=53.45569, geo_lon=9.99083),
+    BahnhofView(bhf_id="BFBI", bhf_name="Flughafen BER", geo_lat=52.36319, geo_lon=13.44924),
+    BahnhofView(bhf_id="BFRI", bhf_name="Berlin Friedrichstraße", geo_lat=52.52059, geo_lon=13.38692),
+    BahnhofView(bhf_id="BGS", bhf_name="Berlin-Gesundbrunnen", geo_lat=52.54883, geo_lon=13.39886),
+    BahnhofView(bhf_id="BHF", bhf_name="Berlin Ostbahnhof", geo_lat=52.51066, geo_lon=13.43350),
+    BahnhofView(bhf_id="BL", bhf_name="Berlin Hauptbahnhof", geo_lat=52.52709, geo_lon=13.36799),
+    BahnhofView(bhf_id="BLO", bhf_name="Berlin-Lichtenberg", geo_lat=52.50967, geo_lon=13.49653),
+    BahnhofView(bhf_id="BOKR", bhf_name="Berlin Ostkreuz (Stadtbahn-F)", geo_lat=52.51066, geo_lon=13.43350),
+    BahnhofView(bhf_id="BPAF", bhf_name="Berlin Südkreuz", geo_lat=52.47607, geo_lon=13.36617),
+    BahnhofView(bhf_id="BSPD", bhf_name="Berlin-Spandau", geo_lat=52.52974, geo_lon=13.22396),
+    BahnhofView(bhf_id="BPD", bhf_name="Potsdam Hbf", geo_lat=52.39154, geo_lon=13.06670),
+    BahnhofView(bhf_id="BZOO", bhf_name="Berlin Zoologischer Garten", geo_lat=52.50868, geo_lon=13.33343),
+    BahnhofView(bhf_id="EDG", bhf_name="Duisburg Hbf", geo_lat=51.41184, geo_lon=6.77521),
+    BahnhofView(bhf_id="EDO", bhf_name="Dortmund Hbf", geo_lat=51.51797, geo_lon=7.45926),
+    BahnhofView(bhf_id="EE", bhf_name="Essen Hbf", geo_lat=51.45156, geo_lon=7.01968),
+    BahnhofView(bhf_id="FF", bhf_name="Frankfurt (Main) Hbf", geo_lat=50.10225, geo_lon=8.64860),
+    BahnhofView(bhf_id="FFLF", bhf_name="Frankfurt am Main Flughafen Fernbahnhof", geo_lat=50.04567, geo_lon=8.54051),
+    BahnhofView(bhf_id="FMZ", bhf_name="Mainz Hbf", geo_lat=50.00468, geo_lon=8.25569),
+    BahnhofView(bhf_id="HB", bhf_name="Bremen Hbf", geo_lat=53.08556, geo_lon=8.81201),
+    BahnhofView(bhf_id="HH", bhf_name="Hannover Hbf", geo_lat=52.37424, geo_lon=9.74655),
+    BahnhofView(bhf_id="KB", bhf_name="Bonn Hbf", geo_lat=50.73780, geo_lon=7.07361),
+    BahnhofView(bhf_id="KD", bhf_name="Düsseldorf Hbf", geo_lat=51.21993, geo_lon=6.79288),
+    BahnhofView(bhf_id="KK", bhf_name="Köln Hbf", geo_lat=50.94276, geo_lon=6.95903),
+    BahnhofView(bhf_id="KKDZ", bhf_name="Köln Messe/Deutz", geo_lat=50.94082, geo_lon=6.97452),
+    BahnhofView(bhf_id="LL", bhf_name="Leipzig Hbf", geo_lat=51.35830, geo_lon=12.39752),
+    BahnhofView(bhf_id="MH", bhf_name="München Hbf", geo_lat=48.14164, geo_lon=11.54897),
+    BahnhofView(bhf_id="MOP", bhf_name="München Ost Pbf", geo_lat=48.13114, geo_lon=11.61142),
+    BahnhofView(bhf_id="MP", bhf_name="München-Pasing", geo_lat=48.15185, geo_lon=11.44889),
+    BahnhofView(bhf_id="NN", bhf_name="Nürnberg Hbf", geo_lat=49.44956, geo_lon=11.10960),
+    BahnhofView(bhf_id="RF", bhf_name="Freiburg (Breisgau) Hbf", geo_lat=47.99709, geo_lon=7.83991),
+    BahnhofView(bhf_id="RK", bhf_name="Karlsruhe Hbf", geo_lat=48.99192, geo_lon=8.39826),
+    BahnhofView(bhf_id="RKAB", bhf_name="Karlsruhe Albtalbahnhof", geo_lat=48.99170, geo_lon=8.39481),
+    BahnhofView(bhf_id="RM", bhf_name="Mannheim Hbf", geo_lat=49.47944, geo_lon=8.46958),
+    BahnhofView(bhf_id="TS", bhf_name="Stuttgart Hbf", geo_lat=48.78751, geo_lon=9.18642),
+]
+
+
+ID_MAP = {b.bhf_id: b for b in BHF_LIST}
+ID_LIST = [b.bhf_id for b in BHF_LIST]
+NAME_MAP = {b.bhf_name: b for b in BHF_LIST}
