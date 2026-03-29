790677dccd6fb3a71e90c3232b49775acb41db2b
tinkpad <jurinho@hotmail.de>
2026-03-29 19:08:08 +0200

    Format scrape.py + fix return hint

--- backend/src/scrape.py
+++ backend/src/scrape.py
@@ -35,12 +35,13 @@ def _get_station_info(ril100_code: str) -> tuple[str, str]:
     """
     r = requests.get(f"{BASE_URL}/station/{ril100_code}", headers=HEADERS, timeout=10)
     r.raise_for_status()
-    
+
     root = ET.fromstring(r.text)
     station = root.find(".//station")
     if station is None:
         raise ValueError("Bahnhof nicht gefunden.")
     return (station.attrib["eva"], station.attrib["name"])
+
 
 def _get_last_planned_train(eva: str, time=None):
     """
@@ -50,9 +51,9 @@ def _get_last_planned_train(eva: str, time=None):
     """
     if time is None:
         time = datetime.now()
-        
+
     candidates = []
-    
+
     print(f"Request for time: {time}")
     for hour_offset in [0, -1]:
         t = time + timedelta(hours=hour_offset)
@@ -60,31 +61,34 @@ def _get_last_planned_train(eva: str, time=None):
         hour = t.strftime("%H")
         r = requests.get(f"{BASE_URL}/plan/{eva}/{date}/{hour}", headers=HEADERS, timeout=10)
         r.raise_for_status()
-        
+
         root = ET.fromstring(r.text)
-        
-        #iterate over the timetable elements
+
+        # iterate over the timetable elements
         for s in root.findall("s"):
-            train_line  = s.find("tl")
-            arrival     = s.find("ar")
-            
+            train_line = s.find("tl")
+            arrival = s.find("ar")
+
             if train_line is None or train_line.attrib.get("c") not in FERNVERKEHR:
                 continue
             if arrival is None or _parse_time(arrival.attrib.get("pt")) > time:
                 continue
-            candidates.append({
-                "id":               s.attrib.get("id"),
-                "type":             train_line.attrib.get("c"),
-                "train_number":     train_line.attrib.get("n"),
-                "planned_arrival":  _parse_time(arrival.attrib.get("pt"))
-                })
+            candidates.append(
+                {
+                    "id": s.attrib.get("id"),
+                    "type": train_line.attrib.get("c"),
+                    "train_number": train_line.attrib.get("n"),
+                    "planned_arrival": _parse_time(arrival.attrib.get("pt")),
+                }
+            )
 
     if not candidates:
         return None
-    
+
     return sorted(candidates, key=lambda t: t["planned_arrival"], reverse=True)[0]
 
-def _get_possible_delay(eva: str, id: str, planned_arrival:datetime) -> timedelta:
+
+def _get_possible_delay(eva: str, id: str, planned_arrival: datetime) -> ArrivalStatus:
     r = requests.get(f"{BASE_URL}/fchg/{eva}", headers=HEADERS, timeout=10)
     r.raise_for_status()
 
@@ -93,27 +97,28 @@ def _get_possible_delay(eva: str, id: str, planned_arrival:datetime) -> timedelt
         parsed_id = s.attrib.get("id")
         if parsed_id != id:
             continue
-        
+
         print(f"Found change for: {id}\n")
         arrival = s.find("ar")
         if arrival is None:
             print("No changes on arrival for this train")
             return ArrivalStatus()
-        
-        cs = arrival.get('cs')
-        if cs == 'c':
+
+        cs = arrival.get("cs")
+        if cs == "c":
             print("Zug ausgefallen")
             return ArrivalStatus(cancelled=True)
 
         changed_time = arrival.attrib.get("ct")
-        
+
         if changed_time is None:
             print("No changed time --> no delay")
             return ArrivalStatus()
-        
+
         return ArrivalStatus(delay=_parse_time(changed_time) - planned_arrival)
-    
+
     return ArrivalStatus()
+
 
 def get_latest_train(ril100: str) -> TrainView:
     station_info = _get_station_info(ril100)
@@ -124,7 +129,7 @@ def get_latest_train(ril100: str) -> TrainView:
     print(f"Last train: {last_train}\n")
     if not last_train:
         raise ValueError("Did not find any timetable data")
-    delay_info = _get_possible_delay(eva, last_train['id'], last_train['planned_arrival'])
+    delay_info = _get_possible_delay(eva, last_train["id"], last_train["planned_arrival"])
     combined_train_name = f"{last_train['type']} {last_train['train_number']}"
     if delay_info.cancelled:
         print(f"{combined_train_name} was canceleld")
@@ -132,11 +137,13 @@ def get_latest_train(ril100: str) -> TrainView:
     else:
         delay = delay_info.delay.total_seconds() / 60
         print(f"{combined_train_name} has {delay} minutes delay")
-    
-    return TrainView(train_name = combined_train_name, delay = delay)
-    
+
+    return TrainView(train_name=combined_train_name, delay=int(delay))
+
+
 def main():
     get_latest_train("AA")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
